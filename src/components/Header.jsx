0dbcda0ef8eb8a46ef52f6ecc1cf358dfe10f422
Dark-ak <ak9004729@gmail.com>
2023-02-28 13:47:44 +0530

    changes in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,16 @@ import Logo from "../assets/logo.png"
 
 const Header = () => {
     return (
-        <div className="flex justify-between bg-orange-500 items-center p-4 px-8">
+        <div className="flex justify-between bg-blue-700 items-center p-4 px-8">
             <div>
                 <img src={Logo} alt="" className="w-28" />
             </div>
 
-            <div>
+            <div className="animate-fade-in">
                 <ul className="flex gap-3 items-center">
-                    <li className="text-white font-bold text-lg hover:bg-white hover:text-black cursor-pointer py-2 px-3 rounded-full">Home</li>
-                    <li className="text-white font-bold text-lg hover:bg-white hover:text-black cursor-pointer py-2 px-3 rounded-full">About</li>
-                    <li className="text-white font-bold text-lg hover:bg-white hover:text-black cursor-pointer py-2 px-3 rounded-full">Contact</li>
+                    <li className="text-white font-bold text-lg hover:bg-white hover:text-black cursor-pointer py-2 px-3 rounded-full transition duration-300 ease-in-out">Home</li>
+                    <li className="text-white font-bold text-lg hover:bg-white hover:text-black cursor-pointer py-2 px-3 rounded-full transition duration-300 ease-in-out">About</li>
+                    <li className="text-white font-bold text-lg hover:bg-white hover:text-black cursor-pointer py-2 px-3 rounded-full transition duration-300 ease-in-out">Contact</li>
                 </ul>
             </div>
         </div>
